6ef2d832d7fd9e878323a6876f2c9aa0d98ad840
jagoda2309 <47605818+jagoda2309@users.noreply.github.com>
2019-09-19 12:29:52 +0200

    Dodaj pliki projektów.

---
+++ changetext/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changetext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napisz coś:");
+            var tekst = Console.ReadLine();
+            int lenght = tekst.Length;
+            if (tekst.Length >20)
+            {
+                Console.WriteLine(tekst.Remove(10) + "...");
+                Console.Read();
+
+            }
+            else
+            {
+                Console.WriteLine(tekst);
+                Console.Read();
+            }
+            
+        }
+    }
+}
